a0612a7297621767c995bfb911fc8f0d7ab8e013
AndreXVR <xavier.andre256@gmail.com>
2024-07-10 19:08:16 -0400

    Fixed and renamed build script

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@
 cd ~/aosp # muda para o diretorio aosp localizado na home
 source build/envsetup.sh # carrega as ferramentas para configurar nosso ambiente de desenvolvimento
 lunch sdk_phone_x86_64-userdebug
-emulator
