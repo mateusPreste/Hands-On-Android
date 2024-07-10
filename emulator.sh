ba76c606faf4b803198ba0f9354890775ddf81b3
AndreXVR <xavier.andre256@gmail.com>
2024-07-10 18:57:46 -0400

    Created bash script for emulator

---
+++ emulator.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ~/aosp # muda para o diretorio aosp localizado na home
+source build/envsetup.sh # carrega as ferramentas para configurar nosso ambiente de desenvolvimento
+lunch sdk_phone_x86_64-userdebug
